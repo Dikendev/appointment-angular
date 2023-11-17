ff7dbc5da7dc313c13f8dd2faae792ad4363957b
Dikendev <diken.dev@gmail.com>
2023-11-16 23:18:18 -0300

    add booking list and interfaces

--- src/app/booking-list/booking-list.component.ts
+++ src/app/booking-list/booking-list.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpServiceService } from '../services/http-service.service';
+
+export interface Bookings {
+  client: Client;
+  service: Service;
+  clientId: number;
+  finishAt: Date;
+  id: number;
+  serviceId: number;
+  startAt: Date;
+  total: number;
+  userId: number;
+}
+
+export interface Client {
+  id: number;
+  name: string;
+  email: string;
+  profilePicture: string;
+  phoneNumber: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface Service {
+  id: number;
+  name: string;
+  price: number;
+  requiredTimeMin: number;
+  serviceImage: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 @Component({
   selector: 'app-booking-list',
   templateUrl: './booking-list.component.html',
-  styleUrls: ['./booking-list.component.css']
+  styleUrls: ['./booking-list.component.css'],
 })
-export class BookingListComponent {
+export class BookingListComponent implements OnInit {
+  bookings!: Bookings[];
 
+  constructor(private backendService: HttpServiceService) {}
+  ngOnInit(): void {
+    this.getBookingData();
+  }
+
+  getBookingData(): void {
+    this.backendService.bookingData$.subscribe(
+      (bookings) => (this.bookings = bookings)
+    );
+  }
 }
