3f54550e1a99e7e9a6ee7c26d616e1df8e9aa9f7
Dikendev <diken.dev@gmail.com>
2023-11-16 23:17:49 -0300

    add http request to backend

--- src/app/services/http-service.service.ts
+++ src/app/services/http-service.service.ts
@@ -1,35 +1,68 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, from, map, tap, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, tap } from 'rxjs';
 import { environment } from 'src/environment/environment';
+
+export interface ServiceData {
+  id: number;
+  name: string;
+  price: number;
+  requiredTimeMin: number;
+  serviceImage?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpServiceService {
+  private serviceData = new Subject<any>();
+  serviceData$ = this.serviceData.asObservable();
+  private bookingData = new Subject<any>();
+  bookingData$ = this.bookingData.asObservable();
+
   constructor(private httpClient: HttpClient) {}
 
-  getServices() {
+  services(): Observable<any> {
     const url = environment.backendApi;
-    return this.httpClient.get(`${url}/service`);
+    return this.httpClient.get<Object[]>(`${url}/service`).pipe(
+      tap((data) => {
+        console.log('data', data);
+        this.serviceData.next(data);
+      })
+    );
   }
 
-  getServicesTeste(): Observable<any> {
+  bookings(): Observable<any> {
+    const url = environment.backendApi;
+    return this.httpClient.get<Object[]>(`${url}/booking`).pipe(
+      tap((bookings) => {
+        console.log(`bookings`, bookings);
+        this.bookingData.next(bookings);
+      })
+    );
+  }
+
+  deleteService(serviceName: string): Observable<any> {
     const url = environment.backendApi;
 
-    const data = from(fetch(`${url}/service/`));
+    return this.httpClient
+      .delete<any>(`${url}/service/delete/${serviceName}`)
+      .pipe(
+        tap((data) => {
+          console.log('data', data);
+        })
+      );
+  }
 
-    data.subscribe({
-      next(response) {
-        console.log(response);
-      },
-      error(err) {
-        console.log(err);
-      },
-      complete() {
-        console.log('Completed');
-      },
-    });
-    return data;
+  createService(body: any) {
+    const url = environment.backendApi;
+
+    return this.httpClient.post(`${url}/service/`, body).pipe(
+      tap((data) => {
+        console.log('data', data);
+      })
+    );
   }
 }
