94f2547b65315fb7bd23b61efcf388c735b438f3
Dikendev <diken.dev@gmail.com>
2024-04-29 23:55:42 -0300

    feat add confirm modal

---
+++ src/app/confirm-modal/confirm-modal.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Data } from '../booking/booking.component';
+
+@Component({
+  selector: 'app-confirm-modal',
+  templateUrl: './confirm-modal.component.html',
+  styleUrls: ['./confirm-modal.component.css'],
+})
+export class ConfirmModalComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Data
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onYesClick(): void {
+    this.dialogRef.close(true);
+  }
+}
